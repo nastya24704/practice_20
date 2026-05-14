8f65a5604dd9d5125020f0e49a282ea4b54dd8cf
nastya24704 <nastazeravina@gmail.com>
2026-05-14 08:48:52 +0700

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -50,12 +50,6 @@ class RomanNumber:
             return "None"
         return self.rom_value
 
-    def __repr__(self) -> str:
-        """Return representation for list output."""
-        if self.rom_value is None:
-            return "None"
-        return self.rom_value
-
     @staticmethod
     def is_roman(value: str) -> bool:
         """
@@ -121,20 +115,26 @@ class RomanNumber:
         
         return True
 
+
 num_1 = RomanNumber('VI')
 print(num_1.rom_value)
 print(num_1.decimal_number())
 print(num_1)
+
 num_2 = RomanNumber('IIII')
 print(num_2.rom_value)
+
 num_3 = RomanNumber('XXIV')
 print(num_3.decimal_number())
+
 num_4 = RomanNumber('QER2')
+
 nums = []
 nums.append(num_1)
 nums.append(num_2)
 nums.append(num_3)
 nums.append(num_4)
+
 print(nums)
-print(RomanNumber.is_roman('MMMCMLXXXVI'))
-print(RomanNumber.is_roman('MMМMMLXXXVI'))
+print(RomanNumber.is_roman('MMMMCLXXXVI'))
+print(RomanNumber.is_roman('MMMMMLXXXVI'))
